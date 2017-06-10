cdce7cc54ea34370146fe60d19d29fb71189f18f
twig <twigwhite@gmail.com>
2017-06-10 12:30:18 -0500

    Updated gitignore to not include .vscode folder, removed Durp.cs, added Whale.cs.

---
+++ Assets/Scripts/Whale.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+public class Whale : MonoBehaviour 
+{
+	private Rigidbody2D _rigidbody;
+
+	private void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+		_rigidbody.gravityScale = 0;
+		_rigidbody.drag = 0.15f;
+	}
+
+	public void Launch(Vector2 direction)
+	{
+		_rigidbody.AddForce(direction, ForceMode2D.Impulse);
+	}
+}
