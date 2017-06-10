51c3dabe33e18458bd6a81b27968d24f64c72764
Kevin Chen <kevin.chen@aviture.us.com>
2017-06-10 14:44:19 -0500

    add simple wind component

---
+++ Assets/Scripts/WindComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindComponent : MonoBehaviour {
+
+    [SerializeField]
+    private Vector2 _windValue = new Vector2(0.1f, 0.05f);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        other.GetComponent<Whale>().Launch(_windValue);
+    }
+}
