3532a8cea56f506645fac70989f7d43a2ccc6644
twig <twigwhite@gmail.com>
2017-06-10 14:21:52 -0500

    Added Prefabs folder with a temp player and whale prefab, added a test level to do level things in, added a LevelManager to keep track of level stuff.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+	// How many whales you have for this level
+	public int _whales;
+	// The player to spawn
+	public GameObject _playerObj;
+	public Transform _playerSpawn;
+	public GameObject _whaleObj;
+	public Transform _whaleSpawn;
+	
+	private int _score;
+
+	private void Start()
+	{
+		GameObject.Instantiate(_playerObj, _playerSpawn.position, _playerSpawn.rotation);
+		GameObject.Instantiate(_whaleObj, _whaleSpawn.position, _whaleSpawn.rotation);
+	}
+
+	public void AddScore(int score)
+	{
+		_score += score;
+	}
+
+	public void RemoveWhale()
+	{
+		_whales -= 1;
+		if (_whales == 0)
+		{
+			Debug.Log("Game Over!");
+		}
+	}
+}
